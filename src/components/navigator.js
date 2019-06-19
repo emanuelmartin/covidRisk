fdbe4454f4a5a3b62991d7534121ac7f2e8a7dcd
Chema <nt693909@iteso.mx>
2019-06-18 19:28:57 -0500

    Actualización navegador

--- src/components/navigator.js
+++ src/components/navigator.js
@@ -1,20 +1,18 @@
 //import React from 'react';
-import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createSwitchNavigator, createAppContainer } from 'react-navigation';
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm';
 import LoadingScreen from './LoadingScreen';
 import HomeScreen from './HomeScreen';
 
-//const AppStack = createStackNavigator({ Home: HomeScreen, SignUp: SignUpForm });
+const AppStack = createStackNavigator({ Login: LoginForm, SignUp: SignUpForm });
 //const AuthStack = createStackNavigator({ LogIn: LoginForm });
 
 const Nav = createAppContainer(createSwitchNavigator(
   {
     AuthLoading: LoadingScreen,
-    Login: LoginForm,
-    SignUp: SignUpForm,
-    Home: HomeScreen
-    //App: AppStack,
+    Home: HomeScreen,
+    App: AppStack
     //Auth: AuthStack,
   },
   {
